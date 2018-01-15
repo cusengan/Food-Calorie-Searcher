f965f4009e19a07199df5ccee8fc52c003a41769
William Truong <lordvinh1@gmail.com>
2018-01-14 20:28:39 -0600

    Submit button now gets the resourceID of the food being searched, need to get the food nutrients and start implementing canvas.js

--- src/app/apiTest.js
+++ src/app/apiTest.js
@@ -1,22 +1,47 @@
 import axios from 'axios';
 const url = "https://trackapi.nutritionix.com/v2/search/item?nix_item_id=570d85c6806817ee1300c785"
-const urlSearch = "https://trackapi.nutritionix.com/v2/search/instant?query=Kale"
+//const urlSearch = "https://trackapi.nutritionix.com/v2/search/instant?query=yogurt"
+const urlTag = "https://apibeta.nutritionix.com/v2/item/644"
+const urlSearch2 = "https://apibeta.nutritionix.com/v2/search?q=yogurt&limit=10&offset=0"
+//const resourceId = "https://apibeta.nutritionix.com/v2/item/xx5wi8Zxo"
 
+var printThis = ''
 
-function getRequest(){
+const urlFindResourceId = "https://apibeta.nutritionix.com/v2/search?q="
+
+function getNutrients(food){
+	var resourceId = urlFindResourceId + food + "&limit=10&offset=0"
 	axios.get(
-	url,
+	resourceId,
 	{headers:{
 		"x-app-id": "3295a486",
 		"x-app-key": "bf445d5862ac6c6213485dec89c1a47f",
-		"x-remote-user-id": 0
+		//"x-remote-user-id": 0
 	}}
 	).then((response) => {
-		console.log("it works");
-		console.log(response.data.foods[0].food_name);
-		console.log(response.data.foods[0].serving_qty);
-		console.log(response.data.foods[0].serving_unit);
+		console.log(response.data.results[0].item_name)
+		console.log(response.data.results[0].resource_id)
+		return response.data.results[0].resource_id
+	}).then((resourceID) => {
+		printThis = resourceID
+	},
+	(error) =>{
+		console.log(error)
+		console.log("bad")
+	});
 
+}
+async function getNutrientsHelper(resourceID){
+	var urlSearch = "https://apibeta.nutritionix.com/v2/item/" + resourceID 
+	axios.get(
+	urlSearch,
+	{headers:{
+		"x-app-id": "3295a486",
+		"x-app-key": "bf445d5862ac6c6213485dec89c1a47f",
+		//"x-remote-user-id": 0
+	}}
+	).then((response) => {
+		return response
 	},
 	(error) =>{
 		console.log(error)
@@ -25,10 +50,11 @@ function getRequest(){
 }
 
 document.addEventListener('DOMContentLoaded', () =>{
-	var button = document.getElementById('search');
+	var button = document.getElementById('search')
 
 	button.addEventListener('click', ()=>{
-		getRequest();
+		getNutrients("yogurt")
+		console.log(printThis)
 	});
 
 });
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import {render} from 'react-dom'
 import Test from './testing.jsx'
+import axios from 'axios'
 import './apiTest'
+
+const urlFindResourceId = "https://apibeta.nutritionix.com/v2/search?q="
 
 class App extends React.Component{
 	
 	constructor(props){
 		super(props)
 		this.state = {
-			food: ''
+			food: '',
+			calories: 0,
+			protein: 0,
+			carbs: 0,
+			fat: 0,
+			resourceID: 'stuff',
+			picture: '',
+			showState: false
 		}
-
 		this.handleChange = this.handleChange.bind(this)
 		this.handleSubmit = this.handleSubmit.bind(this)
 	}
@@ -22,7 +31,33 @@ class App extends React.Component{
 	}
 
 	handleSubmit(event){
-		alert('A food was submitted: ' + this.state.food)
+		var resourceId = urlFindResourceId + this.state.food + "&limit=10&offset=0"
+		axios.get(
+		resourceId,
+		{headers:{
+			"x-app-id": "3295a486",
+			"x-app-key": "bf445d5862ac6c6213485dec89c1a47f",
+			//"x-remote-user-id": 0
+		}}
+		).then((response) => {
+			console.log(response.data.results[0].item_name)
+			console.log(response.data.results[0].resource_id)
+			return response.data.results[0].resource_id
+		}).then((resourceId) => {
+			this.setState({
+				resourceID: resourceId,
+				showState: true
+			})
+		},
+		(error) =>{
+			console.log(error)
+			console.log("bad")
+			this.setState({
+				resourceID: 'Error',
+				showState: false
+			})
+		});
+
 		event.preventDefault()
 	}
 
@@ -33,7 +68,7 @@ class App extends React.Component{
 				<input type="text" value={this.state.food} onChange={this.handleChange}/>
 				<input type="submit" value="Submit" onClick={this.handleSubmit}/>
 				<button id="search"> Click here!</button>
-				<Test text="we own"/>
+				{this.state.showState && <Test text={this.state.resourceID}/>}
 			</div>
 		);
 	}
